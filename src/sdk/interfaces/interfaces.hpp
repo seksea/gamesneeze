8c17c95e1a8e5b84d19bc57058ea8ba2d13f94ae
will <whyireland@protonmail.com>
2020-11-18 19:23:16 +0000

    test by getting engineClient interface

--- src/sdk/interfaces/interfaces.hpp
+++ src/sdk/interfaces/interfaces.hpp
@@ -5,6 +5,9 @@ namespace Interfaces {
     bool init();
     bool unload();
 
+
+    
+
     typedef void* (*InstantiateInterfaceFn)();
     // https://github.com/ValveSoftware/source-sdk-2013/blob/0d8dceea4310fde5706b3ce1c70609d72a38efdf/sp/src/public/tier1/interface.h#L72
     class InterfaceReg {
@@ -21,9 +24,13 @@ namespace Interfaces {
             InterfaceReg* interfaceReg = *reinterpret_cast<InterfaceReg**>(dlsym(lib, "s_pInterfaceRegs"));
             dlclose(lib);
 
-            InterfaceReg* cur;
-            for (cur = interfaceReg; cur; cur = cur->m_pNext) {
-                Log::log(cur->m_pName);
+            // loop through interfaceReg
+            for (InterfaceReg* cur = interfaceReg; cur; cur = cur->m_pNext) {
+                // If current interface equals input name without the 3 version numbers so if an interface version changes we dont have to care
+                if (strstr(cur->m_pName, name) && strlen(cur->m_pName)-3 == strlen(name)) {
+                    Log::log(std::string("Found interface ") + name + " (" + cur->m_pName + ")");
+                    return reinterpret_cast<T*>(cur->m_CreateFn());
+                }
             }
         }
         Log::err(std::string("Failed to find interface ") + name + " -> " + file);
