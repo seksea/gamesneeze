48b6c0398ebf2572e826265e5eaab1cb436d45cb
will <whyireland@protonmail.com>
2020-11-19 09:53:15 +0000

    clientmode interface

--- src/sdk/interfaces/interfaces.cpp
+++ src/sdk/interfaces/interfaces.cpp
@@ -1,10 +1,21 @@
 #include "../../includes.hpp"
 
+typedef IClientMode* (*getClientModeFn)();
+
 bool Interfaces::init() {
     Log::log("initialising interfaces...");
 
     client = getInterface<IBaseClientDLL>("./csgo/bin/linux64/client_client.so", "VClient");
     engine = getInterface<IVEngineClient>("./bin/linux64/engine_client.so", "VEngineClient");
+
+    
+    uintptr_t HudProcessInput = reinterpret_cast<uintptr_t>(getVTable(client)[10]);
+	getClientModeFn getClientMode = reinterpret_cast<getClientModeFn>(getAbsoluteAddress(HudProcessInput + 11, 1, 5));
+    clientMode = getClientMode();
+
+    Log::log("initialised interfacsdes...");
+
+    std::cout << clientMode;
 
     Log::log("initialised interfaces...");
     return true;
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -6,7 +6,15 @@ namespace Log {
     void err(std::string str);
 }
 
+inline uintptr_t getAbsoluteAddress(uintptr_t ptr, int offset, int size) {
+	return ptr + *reinterpret_cast<int32_t*>(ptr + offset) + size;
+};
+
+inline void**& getVTable(void* c, size_t offset = 0) {
+	return *reinterpret_cast<void***>((size_t)c + offset);
+}
+
 template <typename T>
-inline T getVirtualFunc( void* c, unsigned long i, unsigned long offset = 0) {
-    return reinterpret_cast<T>((*reinterpret_cast<void***>((unsigned long)c + offset))[i]);
+inline T getVirtualFunc(void* c, size_t i, size_t offset = 0) {
+    return reinterpret_cast<T>(getVTable(c, offset)[i]);
 }
