e99e61c17d19bd0dee9b351c2d3efa40b7a838ea
sekc <sekc@sekc.me>
2021-01-05 14:22:03 +0000

    added fps counter

--- src/core/menu/overlay.cpp
+++ src/core/menu/overlay.cpp
@@ -1,5 +1,7 @@
 #include "../../includes.hpp"
 
+float framerate;
 void Menu::drawOverlay(ImDrawList* drawList) {
-    drawList->AddText(ImVec2(3, 3), ImColor(255, 255, 255, 255), "gamesneeze-linux");
+    framerate = 0.9 * framerate + ( 0.1 ) * Interfaces::globals->absoluteframetime;
+    drawList->AddText(ImVec2(3, 3), ImColor(255, 255, 255, 255), (std::string("gamesneeze-linux fps:") + std::to_string((int)(1.f/framerate))).c_str());
 }
