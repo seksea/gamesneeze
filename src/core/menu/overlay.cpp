2a7ba89eb56ee707f3cd9260b1a517fc8a325cdf
sekc <sekc@sekc.me>
2021-01-17 19:21:03 +0000

    changed watermark to black shadow

--- src/core/menu/overlay.cpp
+++ src/core/menu/overlay.cpp
@@ -5,15 +5,7 @@ void Menu::drawOverlay(ImDrawList* drawList) {
     framerate = 0.9 * framerate + ( 0.1 ) * Interfaces::globals->absoluteframetime;
     char watermarkText[64]; 
     sprintf(watermarkText, "gamesneeze-linux fps: %d", (int)(1.f/framerate));
-    // Hacky way to do black outline
-    drawList->AddText(ImVec2(4, 2), ImColor(0, 0, 0, 255), watermarkText);
-    drawList->AddText(ImVec2(3, 2), ImColor(0, 0, 0, 255), watermarkText);
-    drawList->AddText(ImVec2(2, 2), ImColor(0, 0, 0, 255), watermarkText);
-    drawList->AddText(ImVec2(4, 3), ImColor(0, 0, 0, 255), watermarkText);
-    //drawList->AddText(ImVec2(3, 3), ImColor(255, 255, 255, 255), watermarkText);
-    drawList->AddText(ImVec2(2, 3), ImColor(0, 0, 0, 255), watermarkText);
+    // Hacky way to do black shadow but it works
     drawList->AddText(ImVec2(4, 4), ImColor(0, 0, 0, 255), watermarkText);
-    drawList->AddText(ImVec2(3, 4), ImColor(0, 0, 0, 255), watermarkText);
-    drawList->AddText(ImVec2(2, 4), ImColor(0, 0, 0, 255), watermarkText);
     drawList->AddText(ImVec2(3, 3), ImColor(255, 255, 255, 255), watermarkText);
 }
