840133f0e2a5002e9b7c0ebb130566dff7be8f96
sekc <sekc@sekc.me>
2021-02-05 11:35:54 +0000

    updated watermark to use imgui framerate

--- src/core/menu/overlay.cpp
+++ src/core/menu/overlay.cpp
@@ -3,9 +3,8 @@
 float framerate;
 void Menu::drawOverlay(ImDrawList* drawList) {
     Globals::drawList = drawList;
-    framerate = 0.9 * framerate + ( 0.1 ) * Interfaces::globals->absoluteframetime;
     char watermarkText[64]; 
-    sprintf(watermarkText, "gamesneeze-linux fps: %d", (int)(1.f/framerate));
+    sprintf(watermarkText, "gamesneeze-linux fps: %.1f", ImGui::GetIO().Framerate);
     // Hacky way to do black shadow but it works
     Globals::drawList->AddText(ImVec2(4, 4), ImColor(0, 0, 0, 255), watermarkText);
     Globals::drawList->AddText(ImVec2(3, 3), ImColor(255, 255, 255, 255), watermarkText);
