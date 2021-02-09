6d7256035c69e48317240f96d4f957d50584c0ca
tango1337 <leicesterpower.ben@gmail.com>
2021-02-09 20:59:11 +0000

    mmm

--- src/core/features/fastduck.cpp
+++ src/core/features/fastduck.cpp
@@ -3,8 +3,8 @@
 void Features::FastDuck::createMove(CUserCmd* cmd) {
     if (CONFIGBOOL("Misc>Misc>Fast Duck")) {
         if (Globals::localPlayer) {
-            if (Globals::localPlayer->helath() > 0) {
-                cmd->buttons |= IN_BULLRUSH;
+            if (Globals::localPlayer->health() > 0) {
+                cmd->buttons |= (1 << 22);
             }
         }
     }
