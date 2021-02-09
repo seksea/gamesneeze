c6000dc8779cfc2d4ae68522457313c5685f4766
tango1337 <leicesterpower.ben@gmail.com>
2021-02-09 21:00:10 +0000

    mmm

--- src/core/hooks/createmove.cpp
+++ src/core/hooks/createmove.cpp
@@ -6,6 +6,7 @@ bool Hooks::CreateMove::hook(void* thisptr, float flInputSampleTime, CUserCmd* c
 
     Features::RankReveal::createMove(cmd);
     Features::AutoHop::createMove(cmd);
+    Features::FastDuck::createMove(cmd);
 
     if (Features::AutoDefuse::shouldDefuse) {
         cmd->buttons |= (1 << 5);
