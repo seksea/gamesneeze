56ab6768f09804330521c8c52a8be6dcb33e355c
sekc <sekc@sekc.me>
2021-02-12 03:53:28 +0000

    undid last commit bc im retarded and tired, will do it tomorrow its 4am

--- src/sdk/interfaces/ibaseclientdll.hpp
+++ src/sdk/interfaces/ibaseclientdll.hpp
@@ -4,6 +4,27 @@
 
 struct RecvProp;
 
+struct DVariant {
+	union
+	{
+		float m_Float;
+		long m_Int;
+		char *m_pString;
+		void *m_pData;
+		float m_Vector[3];
+		int64_t m_Int64;
+	};
+
+	int m_Type;
+};
+
+struct CRecvProxyData {
+	const RecvProp* m_pRecvProp;
+	DVariant m_Value;
+	int m_iElement;
+	int m_ObjectID;
+};
+
 struct RecvTable{
 	RecvProp *m_pProps;
 	int m_nProps;
@@ -11,6 +32,26 @@ struct RecvTable{
 	char *m_pNetTableName;
 	bool m_bInitialized;
 	bool m_bInMainList;
+};
+
+typedef void (*RecvVarProxyFn) (const CRecvProxyData *pData, void *pStruct, void *pOut);
+
+struct RecvProp {
+	char *m_pVarName;
+	int m_RecvType;
+	int m_Flags;
+	int m_StringBufferSize;
+	bool m_bInsideArray;
+	const void *m_pExtraData;
+	RecvProp *m_pArrayProp;
+	void *m_ArrayLengthProxy;
+	RecvVarProxyFn m_ProxyFn;
+	void *m_DataTableProxyFn;
+	RecvTable *m_pDataTable;
+	int m_Offset;
+	int m_ElementStride;
+	int m_nElements;
+	const char *m_pParentArrayPropName;
 };
 
 class IClientNetworkable;
