a920688a57c35cc9cbd1b7f1ae20dc8b8f129375
tango1337 <leicesterpower.ben@gmail.com>
2021-02-18 14:36:09 +0000

    ok now be quiet

--- src/core/features/autohop.cpp
+++ src/core/features/autohop.cpp
@@ -4,7 +4,7 @@ void Features::AutoHop::createMove(CUserCmd* cmd) {
     if (CONFIGBOOL("Misc>Misc>Movement>Auto Hop")) {
             if (!(Globals::localPlayer->flags() & (1<<0))) { // need to make macro for FL_* 
                 int chanceToHit = rand() % 100 + 1;
-                    if (CONFIGITEM("Misc>Misc>Movement>Auto Hop Hitchance") <= chance) {
+                    if (CONFIGITEM("Misc>Misc>Movement>Auto Hop Hitchance") <= chanceToHit) {
                         cmd->buttons &= ~(1<<1); // need to make macro for IN_* 
                     }
                     else {
