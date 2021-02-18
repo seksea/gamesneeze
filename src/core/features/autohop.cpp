9fd36fae4829b8acef8c7d6e6a0b9f095600b137
tango1337 <leicesterpower.ben@gmail.com>
2021-02-18 13:52:44 +0000

    Added Bhop Chance

--- src/core/features/autohop.cpp
+++ src/core/features/autohop.cpp
@@ -2,7 +2,7 @@
 
 void Features::AutoHop::createMove(CUserCmd* cmd) {
     if (CONFIGBOOL("Misc>Misc>Movement>Auto Hop")) {
-        if (cmd->tick_count % CONFIGINT("Misc>Misc>Movement>Legitness")) {
+        if (cmd->tick_count % CONFIGINT("Misc>Misc>Movement>Legitness" == 0)) {
             if (!(Globals::localPlayer->flags() & (1<<0))) { // need to make macro for FL_* 
                 cmd->buttons &= ~(1<<1); // need to make macro for IN_* 
             }
