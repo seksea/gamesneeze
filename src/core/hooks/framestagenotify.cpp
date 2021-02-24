18e10ddda095de80405231e41faa7249a044b29a
sekc <sekc@sekc.me>
2021-02-24 16:51:22 +0000

    fix framestage crash

--- src/core/hooks/framestagenotify.cpp
+++ src/core/hooks/framestagenotify.cpp
@@ -6,7 +6,7 @@ void Hooks::FrameStageNotify::hook(void* thisptr, FrameStage frame) {
     Features::SkyboxChanger::frameStageNotify(frame);
     Features::RecoilCrosshair::frameStageNotify(frame);
 
-    if (frame == FRAME_START) {
+    if (frame == FRAME_NET_UPDATE_END) {
         cachePlayers();
     }
     original(thisptr, frame);
