4c8d5d4a46449bc45b821c79d303d750688d3ddd
sekc <sekc@sekc.me>
2021-06-07 21:55:23 +0100

    made edge jump code more consistent with rest of project
    removed checks in prePred bc there isn't really a need for them

--- src/core/features/edgejump.cpp
+++ src/core/features/edgejump.cpp
@@ -4,40 +4,17 @@
 int flagsBackup = 0;
 
 void Features::EdgeJump::prePredCreateMove(CUserCmd* cmd) {
-    if (!CONFIGBOOL("Misc>Misc>Movement>Edge Jump"))
-        return;
-
-    if (!Menu::CustomWidgets::isKeyDown(CONFIGINT("Misc>Misc>Movement>Edge Jump Key")))
-        return;
-
-    if (!Globals::localPlayer)
-        return;
-
-    if (Globals::localPlayer->moveType() == MOVETYPE_LADDER)
-        return;
-
-    if (Globals::localPlayer->moveType() == MOVETYPE_NOCLIP)
-        return;
-
-    flagsBackup = Globals::localPlayer->flags();
+    if (Globals::localPlayer)
+        flagsBackup = Globals::localPlayer->flags();
 }
 
 void Features::EdgeJump::postPredCreateMove(CUserCmd* cmd) {
-    if (!CONFIGBOOL("Misc>Misc>Movement>Edge Jump"))
-        return;
-
-    if (!Menu::CustomWidgets::isKeyDown(CONFIGINT("Misc>Misc>Movement>Edge Jump Key")))
-        return;
-
-    if (!Globals::localPlayer)
-        return;
-
-    if (Globals::localPlayer->moveType() == MOVETYPE_LADDER)
-        return;
-
-    if (Globals::localPlayer->moveType() == MOVETYPE_NOCLIP)
-        return;
-
-    if (flagsBackup & FL_ONGROUND && !(Globals::localPlayer->flags() & FL_ONGROUND))
+    if (CONFIGBOOL("Misc>Misc>Movement>Edge Jump") && 
+            Menu::CustomWidgets::isKeyDown(CONFIGINT("Misc>Misc>Movement>Edge Jump Key")) && 
+            Globals::localPlayer && 
+            Globals::localPlayer->moveType() != MOVETYPE_LADDER && 
+            Globals::localPlayer->moveType() != MOVETYPE_NOCLIP && 
+            flagsBackup & FL_ONGROUND && 
+            !(Globals::localPlayer->flags() & FL_ONGROUND))
         cmd->buttons |= IN_JUMP;
 }
