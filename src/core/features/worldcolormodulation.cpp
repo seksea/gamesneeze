ad2eec864a0b7efb4f0987b2768e16158da32a04
veasman <charlton.moren@gmail.com>
2021-06-09 02:38:13 -0500

    Add skybox color mod

--- src/core/features/worldcolormodulation.cpp
+++ src/core/features/worldcolormodulation.cpp
@@ -7,5 +7,9 @@ void Features::WorldColorModulate::updateColorModulation() {
             material->AlphaModulate(CONFIGCOL("Visuals>World>World>World Color Modulation").Value.w);
             material->ColorModulate(CONFIGCOL("Visuals>World>World>World Color Modulation").Value.x, CONFIGCOL("Visuals>World>World>World Color Modulation").Value.y, CONFIGCOL("Visuals>World>World>World Color Modulation").Value.z);
         }
+        if (material && strstr(material->GetTextureGroupName(), "SkyBox")) {
+            material->AlphaModulate(CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.w);
+            material->ColorModulate(CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.x, CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.y, CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.z);
+        }
     }
 }
