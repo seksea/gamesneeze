d5590cd50daa53068ebeb31d7d72da40efa4f5cc
veasman <charlton.moren@gmail.com>
2021-06-09 23:46:12 -0500

    Fix StaticProp mat group not changing colors

--- src/core/features/worldcolormodulation.cpp
+++ src/core/features/worldcolormodulation.cpp
@@ -3,10 +3,15 @@
 void Features::ColorModulation::updateColorModulation() {
     for(auto i = Interfaces::materialSystem->FirstMaterial(); i != Interfaces::materialSystem->InvalidMaterial(); i = Interfaces::materialSystem->NextMaterial(i)) {
         IMaterial* material = Interfaces::materialSystem->GetMaterial(i);
-        if (material && strstr(material->GetTextureGroupName(), "World")) {
+        if (material && (strstr(material->GetTextureGroupName(), "World"))) {
             material->AlphaModulate(CONFIGCOL("Visuals>World>World>World Color Modulation").Value.w);
             material->ColorModulate(CONFIGCOL("Visuals>World>World>World Color Modulation").Value.x, CONFIGCOL("Visuals>World>World>World Color Modulation").Value.y, CONFIGCOL("Visuals>World>World>World Color Modulation").Value.z);
         }
+        if (material && (strstr(material->GetTextureGroupName(), "StaticProp"))) {
+            material->AlphaModulate(CONFIGCOL("Visuals>World>World>World Color Modulation").Value.w);
+            material->ColorModulate(CONFIGCOL("Visuals>World>World>World Color Modulation").Value.x / 2, CONFIGCOL("Visuals>World>World>World Color Modulation").Value.y / 2, CONFIGCOL("Visuals>World>World>World Color Modulation").Value.z / 2);
+        }
+
         if (material && strstr(material->GetTextureGroupName(), "SkyBox")) {
             material->AlphaModulate(CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.w);
             material->ColorModulate(CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.x, CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.y, CONFIGCOL("Visuals>World>World>SkyBox Color Modulation").Value.z);
