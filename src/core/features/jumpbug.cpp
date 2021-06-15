02c6c53d4806c71a0057e7b0386c5558bc7aa4a1
veasman <charlton.moren@gmail.com>
2021-06-15 15:58:20 -0500

    Keybind and improvements

--- src/core/features/jumpbug.cpp
+++ src/core/features/jumpbug.cpp
@@ -3,9 +3,12 @@
 
 void Features::JumpBug::createMove(CUserCmd* cmd) {
      if (CONFIGBOOL("Misc>Misc>Movement>Jump Bug") &&
-             Interfaces::inputSystem->IsButtonDown(KEY_SPACE) &&
-             (Globals::localPlayer->flags() & FL_ONGROUND || Globals::localPlayer->flags() & FL_PARTIALGROUND) &&
-             !(Features::EdgeJump::flagsBackup & FL_ONGROUND || Features::EdgeJump::flagsBackup & FL_PARTIALGROUND)) {
+         Interfaces::inputSystem->IsButtonDown(KEY_SPACE) &&
+         Menu::CustomWidgets::isKeyDown(CONFIGINT("Misc>Misc>Movement>Jump Bug Key")) &&
+         Globals::localPlayer->moveType() != MOVETYPE_LADDER &&
+         Globals::localPlayer->moveType() != MOVETYPE_NOCLIP &&
+         (Globals::localPlayer->flags() & FL_ONGROUND || Globals::localPlayer->flags() & FL_PARTIALGROUND) &&
+         !(Features::EdgeJump::flagsBackup & FL_ONGROUND || Features::EdgeJump::flagsBackup & FL_PARTIALGROUND)) {
          cmd->buttons |= IN_DUCK;
          cmd->buttons &= ~IN_JUMP;
      }
