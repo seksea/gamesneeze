bdde651135a2c13b4ad8069fcca944dc67707ca4
sekc <sekc@sekc.me>
2021-06-17 12:24:21 +0100

    fix skybox changer no 3d skybox logic

--- src/core/features/skyboxchanger.cpp
+++ src/core/features/skyboxchanger.cpp
@@ -3,13 +3,15 @@
 
 void Features::SkyboxChanger::frameStageNotify(FrameStage frame) {
     static ConVar* r_3dsky = Interfaces::convar->FindVar("r_3dsky");
-    if (frame == FRAME_NET_UPDATE_POSTDATAUPDATE_END && CONFIGINT("Visuals>World>World>Skybox")) {
-        Offsets::setNamedSkybox((CONFIGINT("Visuals>World>World>Skybox") != 1) ? skyboxes[CONFIGINT("Visuals>World>World>Skybox")] : "sky_l4d_rural02_ldr");
-        if (r_3dsky) {
-            r_3dsky->SetValue(0);
+    if (frame == FRAME_NET_UPDATE_POSTDATAUPDATE_END) {
+        if (CONFIGINT("Visuals>World>World>Skybox")) {
+            Offsets::setNamedSkybox((CONFIGINT("Visuals>World>World>Skybox") != 1) ? skyboxes[CONFIGINT("Visuals>World>World>Skybox")] : "sky_l4d_rural02_ldr");
+            if (r_3dsky) {
+                r_3dsky->SetValue(0);
+            }
         }
-    }
-    else if (r_3dsky) {
-        r_3dsky->SetValue(1);
+        else if (r_3dsky) {
+            r_3dsky->SetValue(1);
+        }
     }
 }
