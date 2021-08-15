df89e3da024f31372822416a730b0db505c08770
Can SEFEROGLU <can.seferoglu@gmail.com>
2021-08-15 04:45:21 +0300

    Added option to use random build name
    Gets a random build name from the build_names file with the -r (--random_name) argument.

--- toolbox.sh
+++ toolbox.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 gdb="$(dirname "$0")/gdb" # For using a gdb build such as the cathook one (The one included)
-libname="libgamemodeauto.so.0" # Pretend to be gamemode, change this to another lib that may be in /maps (if already using real gamemode, I'd suggest using libMangoHud.so)
+
+if [ -f libname ]; then
+	libname=$(cat libname) # Check if random_name was used
+else
+	libname="libgamemodeauto.so.0" # Pretend to be gamemode, change this to another lib that may be in /maps (if already using real gamemode, I'd suggest using libMangoHud.so)
+fi
+
 csgo_pid=$(pidof csgo_linux64)
 
 # Set to true to compile with clang. (if changing to true, make sure to delete the build directory from gcc)
@@ -14,6 +20,31 @@ fi
 
 rm -rf /tmp/dumps
 mkdir -p --mode=000 /tmp/dumps
+
+function random_name {
+	# Get a Random name from the build_names file.
+	libname=$(shuf -n 1 build_names)
+
+	# In case this file exists, get another one. ( checked it works )
+	while [ -f "/usr/lib/${libname}" ] || grep -q ${libname} /proc/$csgo_pid/maps; do	
+		libname=$(shuf -n 1 build_names)
+	done
+	
+	if [ -f libname ]; then
+	    filename=$(cat libname)
+	    rm "$filename"
+	    if [ -f "/usr/lib/${filename}" ]; then
+	    	echo -e "Note: Old file found at /usr/lib/${filename}\n"
+		#sudo rm "/usr/lib/${filename}"
+
+	    fi
+	    chmod 660 libname
+	    mv libname libname_old
+	fi
+
+	echo $libname > libname
+	chmod 400 libname # Make the libname read-only for safety.
+}
 
 function unload {
     echo "Unloading cheat..."
@@ -113,6 +144,10 @@ case $keys in
         build_debug
         shift
         ;;
+    -r|--random_name)
+        random_name
+        shift
+        ;;
     -p|--pull)
         pull
         shift
@@ -129,6 +164,7 @@ Toolbox script for gamesneeze the beste lincuck cheat 2021
 | -ld (--load_debug)   | Load/inject the cheat and debug via gdb.     |
 | -b (--build)         | Build to the build/ dir.                     |
 | -bd (--build_debug)  | Build to the build/ dir as debug.            |
+| -r (--random_name)   | Use random build name.                       |
 | -p (--pull)          | Update the cheat.                            |
 | -h (--help)          | Show help.                                   |
 =======================================================================
