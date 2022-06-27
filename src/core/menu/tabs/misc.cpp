0cc77ded0a1bb634b0b8698f7fce60da4a53ed1c
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2022-06-27 13:55:51 +0300

    add myself to contributors

--- src/core/menu/tabs/misc.cpp
+++ src/core/menu/tabs/misc.cpp
@@ -135,7 +135,7 @@ void Menu::drawMiscTab() {
             ImGui::Columns(1);
             ImGui::TextDisabled("Credits!");
             if (ImGui::IsItemHovered())
-                ImGui::SetTooltip("sekc (of course)\nAll other contributors on GitHub (xXx-sans-xXx, luk1337, cristeigabriel, crazily, dave (@dweee), keenan, u2ooS, tango1337, nigma1337, vampur, all and any other contributors, and allbombson)\nand ocornut for his great ImGui UI framework");
+                ImGui::SetTooltip("sekc (of course)\nAll other contributors on GitHub (xXx-sans-xXx, luk1337, cristeigabriel, crazily, dave (@dweee), keenan, u2ooS, tango1337, nigma1337, vampur, jovvik, all and any other contributors, and allbombson)\nand ocornut for his great ImGui UI framework");
             ImGui::EndTabItem();
         }
 
